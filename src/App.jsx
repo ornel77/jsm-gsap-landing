fcebddeaab966a5a2152caaadcb6cb7f04db8e1c
ornel77 <ornella.tomboza@gmail.com>
2025-12-16 16:12:14 +0100

    implement Navbar ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import gsap from 'gsap'
 import {ScrollTrigger, SplitText} from 'gsap/all'
+import Navbar from './components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <div className="flex-center h-screen">App</div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 export default App
